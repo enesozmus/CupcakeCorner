f0e4af5b96a7fd4ed97676e424e5e629acead47d
enesozmus <ensozmus@gmail.com>
2024-03-19 18:40:42 +0300

    Create a observable class and home page design

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -8,14 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // → We use @Observable classes with @State wrappers in views.
+    @State private var order = Order()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        // → A view that displays a root view and enables you to present additional views over the root view.
+        //→ Use a navigation stack to present a stack of views over a root view.
+        NavigationStack {
+            Form {
+                
+                // ...
+                Section {
+                    Picker("Select your cake type", selection: $order.type) {
+                        ForEach(Order.types.indices, id: \.self) {
+                            Text(Order.types[$0])
+                        }
+                    }
+                    Stepper("Number of cakes: \(order.quantity)", value: $order.quantity, in: 1...15)
+                }
+                
+                // ...
+                Section {
+                    Toggle("Any special requests?", isOn: $order.specialRequestEnabled)
+                    
+                    if order.specialRequestEnabled  {
+                        Toggle("Add extra frosting", isOn: $order.extraFrosting)
+                        Toggle("Add extra sprinkles", isOn: $order.addSprinkles)
+                    }
+                }
+                
+                // ...
+                Section {
+                    NavigationLink("Delivery details") {
+                        Text("...")
+                        // AddressView(order: order)
+                    }
+                }
+                
+            }
+            .navigationTitle("Cupcake Corner")
         }
-        .padding()
     }
 }
 
